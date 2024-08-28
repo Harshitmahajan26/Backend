c7900faa1d63ff7c00048f8a1e679ae2bcfdafc6
Harshitmahajan26 <123460606+Harshitmahajan26@users.noreply.github.com>
2024-08-28 22:55:48 +0530

    28-08-24 mongosh

---
+++ script.js
@@ -0,0 +1,11 @@
+// mongosh
+
+// use name
+// db.createCollection("new")
+// db.new.insertOne({name:"ankur", age:28, class:"b9"})
+// db.new.insertMany([
+//     { "name": "dksdf", "age": 40, "marks": 99, "sub": "eng" },
+// ... { "name": "ofdge", "age": 20, "marks": 80, "sub": "math" },
+// ... { "name": "dfgae", "age": 30, "marks": 70, "sub": "dso" },
+// ... { "name": "jjksd", "age": 50, "marks": 60, "sub": "dfgad" }
+// ])
